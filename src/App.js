42bf6b39b51ebfb3fe10410b3cd790b34ad5c77e
tosaric <tosaric@fesb.hr>
2021-04-28 23:07:20 +0200

    Fix api call and implement list of colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import styled from 'styled-components'
 import axios from 'axios'
+import { useEffect, useState } from "react"
 
 const Button = styled.button`
   color: #fff;
@@ -17,33 +18,39 @@ const Button = styled.button`
     background-color: #003759;
   }
 `
+const List = styled.ul`
+  padding-top: 50px;
+  text-align: left;
+`
 
 function App() {
-
-  const onChangeColor = async (e) => {
-
-    const result = await axios.get("/api",
-    {
-      header: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept, Accept-Language, X-Authorization"
-      }
-    })
-
+  let [listOfColors, setColor] = useState([]);
+  
+  const onChangeColor = (e) => {
+    
+    axios.get("https://www.colr.org/json/color/random",
+    { crossdomain: true })
     .then(response => {
         console.log("Success =>", response.data);
         
         console.log(response.data.colors[0].hex)
-   
+
         e.target.style.color = response.data.colors[0].hex
+
+        if(!listOfColors.includes(response.data.colors[0].hex))
+        {
+          setColor(...listOfColors, response.data.colors[0].hex)
+        }
     })
     .catch(error => {
         console.log("Error =>", error);
-     })
-
-   
+    })
   }
+  
+
+  useEffect(() => {
+   
+  }, [])
 
   return (
     <div className="App">
@@ -51,6 +58,11 @@ function App() {
         <Button id="button-color" onClick={onChangeColor} >
           Change color
         </Button>
+        <List>
+          { listOfColors !== undefined ? listOfColors.map(c =>
+            <li style={{itemColor: c}} >{"" + c}</li>)
+           : <></> }
+        </List>
       </header>
     </div>
   );
